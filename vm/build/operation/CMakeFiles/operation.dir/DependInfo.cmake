3526b0e36974d17bfb9bd83b3f652201fe8938c2
Aaron SU <aaron.su0424@gmail.com>
2024-03-24 17:44:10 +0100

    remove and move some files to furthur functionality

---
+++ vm/build/operation/CMakeFiles/operation.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/FYMC/vm/operation/src/compare_jump.c" "operation/CMakeFiles/operation.dir/src/compare_jump.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/compare_jump.c.o.d"
+  "/home/user/FYMC/vm/operation/src/conversion.c" "operation/CMakeFiles/operation.dir/src/conversion.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/conversion.c.o.d"
+  "/home/user/FYMC/vm/operation/src/f_arithmetic_logic.c" "operation/CMakeFiles/operation.dir/src/f_arithmetic_logic.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/f_arithmetic_logic.c.o.d"
+  "/home/user/FYMC/vm/operation/src/lecture.c" "operation/CMakeFiles/operation.dir/src/lecture.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/lecture.c.o.d"
+  "/home/user/FYMC/vm/operation/src/load.c" "operation/CMakeFiles/operation.dir/src/load.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/load.c.o.d"
+  "/home/user/FYMC/vm/operation/src/move.c" "operation/CMakeFiles/operation.dir/src/move.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/move.c.o.d"
+  "/home/user/FYMC/vm/operation/src/output.c" "operation/CMakeFiles/operation.dir/src/output.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/output.c.o.d"
+  "/home/user/FYMC/vm/operation/src/s_arithmetic_logic.c" "operation/CMakeFiles/operation.dir/src/s_arithmetic_logic.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/s_arithmetic_logic.c.o.d"
+  "/home/user/FYMC/vm/operation/src/store.c" "operation/CMakeFiles/operation.dir/src/store.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/store.c.o.d"
+  "/home/user/FYMC/vm/operation/src/tools.c" "operation/CMakeFiles/operation.dir/src/tools.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/tools.c.o.d"
+  "/home/user/FYMC/vm/operation/src/u_arithmetic_logic.c" "operation/CMakeFiles/operation.dir/src/u_arithmetic_logic.c.o" "gcc" "operation/CMakeFiles/operation.dir/src/u_arithmetic_logic.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vm/build/operation/CMakeFiles/operation.dir/src/tools.c.o.d
@@ -0,0 +1,21 @@
+operation/CMakeFiles/operation.dir/src/tools.c.o: \
+ /home/user/FYMC/vm/operation/src/tools.c /usr/include/stdc-predef.h \
+ /home/user/FYMC/vm/operation/include/tools.h \
+ /home/user/FYMC/vm/operation/../core/type.h \
+ /home/user/FYMC/vm/operation/../core/cpu.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /home/user/FYMC/vm/operation/../core/type.h
