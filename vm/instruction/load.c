9af8cfe7827b5484a3c1aa42842192c17f1f2862
shani <c.ztoti.2001.03@gmail.com>
2024-05-13 20:09:07 +0200

    ajout des vérifications dans loadx

--- vm/instruction/load.c
+++ vm/instruction/load.c
@@ -4,6 +4,13 @@
 void loadu(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
+
     DEBUG_PRINT("--------Avant LOADU--------\n");
     DEBUG_PRINT("Le registre %d a pour valeur %ld\n"
                 "Le registre %d a pour valeur %ld\n"
@@ -12,8 +19,8 @@ void loadu(core_t *core)
                 instruction.register_1, core->U[instruction.register_1],
                 instruction.register_2, core->U[instruction.register_2],
                 instruction.register_3, core->U[instruction.register_3],
-                *(u64 *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]));
-    core->U[instruction.register_1] = *(u64 *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]);
+                *(u64 *)&(core->memory[adresse]));
+    core->U[instruction.register_1] = *(u64 *)&(core->memory[adresse]);
     DEBUG_PRINT("--------Après LOADU--------\n");
     DEBUG_PRINT("Le registre %d a pour valeur %ld\n"
                 "Le registre %d a pour valeur %ld\n"
@@ -22,20 +29,32 @@ void loadu(core_t *core)
                 instruction.register_1, core->U[instruction.register_1],
                 instruction.register_2, core->U[instruction.register_2],
                 instruction.register_3, core->U[instruction.register_3],
-                *(u64 *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]));
+                *(u64 *)&(core->memory[adresse]));
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 
 void loads(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
-    core->S[instruction.register_1] = *(i64 *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]);
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
+    core->S[instruction.register_1] = *(i64 *)&(core->memory[adresse]);
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 
 void loadf(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
     DEBUG_PRINT("--------Avant LOADF--------\n");
     DEBUG_PRINT("Le registre %d a pour valeur %f\n"
                 "Le registre %d a pour valeur %f\n"
@@ -44,8 +63,8 @@ void loadf(core_t *core)
                 instruction.register_1, core->F[instruction.register_1],
                 instruction.register_2, core->F[instruction.register_2],
                 instruction.register_3, core->F[instruction.register_3],
-                *(double *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]));
-    core->F[instruction.register_1] = *(double *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]);
+                *(double *)&(core->memory[adresse]));
+    core->F[instruction.register_1] = *(double *)&(core->memory[adresse]);
     DEBUG_PRINT("--------Après LOADF--------\n");
     DEBUG_PRINT("Le registre %d a pour valeur %f\n"
                 "Le registre %d a pour valeur %f\n"
@@ -54,28 +73,43 @@ void loadf(core_t *core)
                 instruction.register_1, core->F[instruction.register_1],
                 instruction.register_2, core->F[instruction.register_2],
                 instruction.register_3, core->F[instruction.register_3],
-                *(double *)&(core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset]));
+                *(double *)&(core->memory[adresse]));
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 void loadv(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
-
-    memcpy(&core->V[instruction.register_1], &core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset], sizeof(u64) * 8);
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(&core->V[instruction.register_1], &core->memory[adresse], sizeof(u64) * 8);
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 void loadt(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
-
-    memcpy(&core->T[instruction.register_1], &core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset], sizeof(i64) * 8);
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(&core->T[instruction.register_1], &core->memory[adresse], sizeof(i64) * 8);
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 
 void loadg(core_t *core)
 {
     instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
-
-    memcpy(&core->G[instruction.register_1], &core->memory[core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset], sizeof(double) * 8);
+    u64 adresse = core->U[instruction.register_2] + core->U[instruction.register_3] + instruction.offset;
+    if (adresse > MAX_MEMORY_SIZE)
+    {
+        printf("Dépassement mémoire!\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(&core->G[instruction.register_1], &core->memory[adresse], sizeof(double) * 8);
     core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
