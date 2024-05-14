959a788dd7fefc3c7ee9a04ce00ac9bdf61ac176
shani <c.ztoti.2001.03@gmail.com>
2024-05-15 00:15:34 +0200

    Ajout de l'instruction mov, vérification non faites

--- vm/instruction/mov.c
+++ vm/instruction/mov.c
@@ -3,6 +3,151 @@
 
 void mov(core_t *core)
 {
+    instruction_t instruction = instruction_new(*(u32 *)&(core->file_buffer[core->IP]));
+    u8 type_register = instruction.offset & 0x10;
+    u8 r1 = instruction.offset & 0xC;
+    u8 r2 = instruction.offset & 0x3;
+
+    switch (type_register)
+    {
+    case 0:
+        // scalaire
+        // U --> 00 S --> 01 F --> 10
+        switch (r1)
+        {
+        case 0:
+            switch (r2)
+            {
+            case 0:
+                core->U[instruction.register_1] = core->U[instruction.register_2];
+                break;
+            case 1:
+                core->U[instruction.register_1] = (u64)core->S[instruction.register_2];
+                break;
+            case 2:
+                core->U[instruction.register_1] = (u64)core->F[instruction.register_2];
+                break;
+            }
+            break;
+
+        case 1:
+            switch (r2)
+            {
+            case 0:
+                core->S[instruction.register_1] = (i64)core->U[instruction.register_2];
+                break;
+            case 1:
+                core->S[instruction.register_1] = core->S[instruction.register_2];
+                break;
+            case 2:
+                core->S[instruction.register_1] = (i64)core->F[instruction.register_2];
+                break;
+            }
+            break;
+
+        case 2:
+            switch (r2)
+            {
+            case 0:
+                core->F[instruction.register_1] = (f64)core->U[instruction.register_2];
+                break;
+            case 1:
+                core->F[instruction.register_1] = (f64)core->S[instruction.register_2];
+                break;
+            case 2:
+                core->F[instruction.register_1] = core->F[instruction.register_2];
+                break;
+            }
+            break;
+        }
+        break;
+
+    case 1:
+        // vecteur
+        // V --> 00 T --> 01 G --> 10
+        switch (r1)
+        {
+        case 0:
+            switch (r2)
+            {
+            case 0:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->V[instruction.register_1][i] = core->V[instruction.register_2][i];
+                }
+                break;
+            case 1:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->V[instruction.register_1][i] = (u64)core->T[instruction.register_2][i];
+                }
+                break;
+            case 2:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->V[instruction.register_1][i] = (u64)core->G[instruction.register_2][i];
+                }
+                break;
+            }
+            break;
+
+        case 1:
+            switch (r2)
+            {
+            case 0:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->T[instruction.register_1][i] = (i64)core->V[instruction.register_2][i];
+                }
+                break;
+            case 1:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->T[instruction.register_1][i] = core->T[instruction.register_2][i];
+                }
+                break;
+            case 2:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->T[instruction.register_1][i] = (i64)core->G[instruction.register_2][i];
+                }
+                break;
+            }
+            break;
+
+        case 2:
+            switch (r2)
+            {
+            case 0:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->G[instruction.register_1][i] = (f64)core->V[instruction.register_2][i];
+                }
+                break;
+            case 1:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->G[instruction.register_1][i] = (f64)core->T[instruction.register_2][i];
+                }
+                break;
+            case 2:
+                for (int i = 0; i < NUMBER_SCALAR_IN_VECTOR_REGISTER; ++i)
+                {
+                    core->G[instruction.register_1][i] = core->G[instruction.register_2][i];
+                }
+                break;
+            }
+            break;
+        }
+        break;
+
+    default:
+        printf("Erreur: Type de registre non valide\n");
+        exit(EXIT_FAILURE);
+        break;
+    }
+
+    core->IP += SIZE_INSTRUCTION_IN_BYTE;
 }
 
 void movu(core_t *core)
